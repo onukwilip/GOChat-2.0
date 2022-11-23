856c14a0739afbb7824425ec2b0eb483c5968516
memmcol-prince <memmcol.prince@gmail.com>
2022-11-23 08:19:13 +0100

    Corrected the backend API domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     // API_DOMAIN: `https://localhost:44358/`,
-    API_DOMAIN: `https://gochatapi-2-0.azurewebsites.net`,
+    API_DOMAIN: `https://gochatapi-2-0.azurewebsites.net/`,
     // CLIENT_DOMAIN: `http://localhost:3000/`,
     CLIENT_DOMAIN: `https://gochat-tau.vercel.app/`,
     CLIENT_ID: `89db7910-aae7-4b7a-9705-d704ceb8af41`,
